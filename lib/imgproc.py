e5b7e9aed0e327907c524d61e367b272ae8989b0
thangnguyenminh <35911722+thangnguyenminh@users.noreply.github.com>
2018-06-10 17:03:41 +0700

    Update imgproc.py

--- lib/imgproc.py
+++ lib/imgproc.py
@@ -1,24 +1,9 @@
 """
 This file is part of Image Harvest.
 
-Image Harvest is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Image Harvest is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Image Harvest.  If not, see <http://www.gnu.org/licenses/>.
-
 .. module:: imgproc
     :platform: Mac, Linux
     :synopsis: Image processing function wrappers to opencv.
-
-.. moduleauthor:: Avi Knecht <avi@kurtknecht.com>
 
 """
 
